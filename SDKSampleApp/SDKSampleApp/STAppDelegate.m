1b58db2d68985c124d2a697a01fa45978d84223a
sjerome <sjerome@paypal.com>
2014-06-20 13:27:08 -0700

    fixed url

--- SDKSampleApp/SDKSampleApp/STAppDelegate.m
+++ SDKSampleApp/SDKSampleApp/STAppDelegate.m
@@ -64,7 +64,7 @@ PPHLoggingDelegate
     
     //Default to using a stage.  The login sample UI will change this value.
 
-    [PayPalHereSDK setBaseAPIURL:[NSURL URLWithString:@"https://stage2mb006.stage.paypal.com/webapps/"]];
+    [PayPalHereSDK setBaseAPIURL:[NSURL URLWithString:@"https://www.stage2mb006.stage.paypal.com/webapps/"]];
    
     /* By default, the SDK has a remote logging facility for warnings and errors. This helps PayPal immensely in
      * diagnosing issues, but is obviously up to you as to whether you want to do remote logging, or perhaps you
