701de8cfbdd25597fcf69a0b78e6822dd37f6019
schandrashekar <schandrashekar@paypal.com>
2014-11-07 14:37:32 -0800

    Adding the iDynamo support in the NOHW version of the sample app

--- SDKSampleApp/STConstants.h
+++ SDKSampleApp/STConstants.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 PayPalHereSDK. All rights reserved.
 //
 
-#define STAGE @"https://www.stage2pph10.stage.paypal.com/webapps/"
+#define STAGE @"https://www.stage2mb023.stage.paypal.com/webapps/"
 #define SANDBOX @"https://www.sandbox.paypal.com/webapps/"
 
 #define IS_IPHONE ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone)
