a2e276276c0b5ebdad1b27e94e422e90cf418f62
Shu <sashar@paypal.com>
2018-02-01 16:41:38 -0800

    Remove 1.6 from master

--- TakePayment/Pods/Headers/Public/PayPalHereSDK/PayPalHereSDK/PPHSDKBaseViewController.h
+++
@@ -1,1 +0,0 @@
-../../../../../../SDK/Debug/PayPalHereSDK.framework/Versions/A/Headers/PPHSDKBaseViewController.h
--- TakePayment/Pods/Target Support Files/Pods-TakePayment/Pods-TakePayment-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TakePayment : NSObject
-@end
-@implementation PodsDummy_Pods_TakePayment
-@end
