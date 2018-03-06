6e50bf026dd55f501ea3a377ff8fa5db24a98309
Shu <sashar@paypal.com>
2018-03-06 12:00:33 -0800

    Match the structure of the public repo

---
+++ PayPalHereSDKv2.podspec
@@ -0,0 +1,36 @@
+Pod::Spec.new do |s|
+  s.name             = "PayPalHereSDKv2"
+  s.version          = "2.0.1201805"
+  s.homepage         = 'https://developer.paypal.com/docs/integration/paypal-here/'
+  s.source           = { :git => 'https://github.com/PayPal-Mobile/ios-here-sdk-dist.git', :tag => "v#{s.version}" }  
+  s.summary          = 'SDK for interfacing with PayPal card readers and mobile payment processing APIs.'
+  s.description      = 'The PayPal Here SDK v2 for iOS provides access to a group of PayPal transaction services which contain an extensive set of point-of-sale functions for merchants.'
+  s.license          = { :type => 'PAYPAL', :file => 'LICENSE.md' }
+  s.authors          = { 'PayPal' => 'DL-PP-PPH-SDK-Admin@paypal.com' }
+  
+
+  s.requires_arc = true
+  s.ios.deployment_target    = '8.1'
+
+  s.xcconfig = {
+    'FRAMEWORK_SEARCH_PATHS' => '$(inherited)',
+    'OTHER_LDFLAGS' => '-weak_library /usr/lib/libstdc++.dylib -lstdc++ -ObjC',
+  }
+
+  s.ios.frameworks = 'AudioToolbox', 'MobileCoreServices', 'Security', 'CFNetwork', 'AVFoundation', 'ExternalAccessory', 'MediaPlayer', 'CoreTelephony', 'Foundation', 'CoreBluetooth', 'SystemConfiguration', 'JavaScriptCore', 'CoreBluetooth', 'UIKit', 'CoreLocation'
+
+  s.default_subspec = 'Debug'
+
+  s.subspec 'Debug' do |sp|
+    sp.vendored_frameworks      = 'RSDK/Debug/PayPalRetailSDK.framework', 'frameworks/PPHSwiper.framework', 'frameworks/PPHR_BLE.framework', 'frameworks/PPHSDK_BLE.framework'
+    sp.ios.resource             = 'RSDK/Debug/PayPalRetailSDK.framework/Versions/A/Resources/**/*'
+  end
+
+  s.subspec 'Release' do |sp|
+    sp.vendored_frameworks      = 'RSDK/Release/PayPalRetailSDK.framework', 'frameworks/PPHSwiper.framework', 'frameworks/PPHR_BLE.framework', 'frameworks/PPHSDK_BLE.framework'
+    sp.ios.resource             = 'RSDK/Release/PayPalRetailSDK.framework/Versions/A/Resources/**/*'
+  end
+
+  s.dependency 'SimpleKeychain', '~> 0.6.1'
+
+end
---
+++ SampleApp/PPHSDKSampleApp/AuthCompletedViewController.swift
@@ -0,0 +1,118 @@
+//
+//  AuthCompletedViewController.swift
+//  PPHSDKSampleApp
+//
+//  Created by Wright, Cory on 1/8/18.
+//  Copyright © 2018 cowright. All rights reserved.
+//
+
+import UIKit
+import PayPalRetailSDK
+
+class AuthCompletedViewController: UIViewController {
+
+    @IBOutlet weak var successMsg: UILabel!
+    @IBOutlet weak var voidAuthBtn: UIButton!
+    @IBOutlet weak var voidCodeViewBtn: UIButton!
+    @IBOutlet weak var voidCodeViewer: UITextView!
+    @IBOutlet weak var captureAuthBtn: UIButton!
+    @IBOutlet weak var captureAuthCodeViewBtn: UIButton!
+    @IBOutlet weak var captureAuthCodeViewer: UITextView!
+    @IBOutlet weak var startOverBtn: UIButton!
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    @IBOutlet weak var voidSuccessLbl: UILabel!
+    
+    var invoice: PPRetailInvoice?
+    var authTransactionNumber: String?
+    var paymentMethod: PPRetailInvoicePaymentMethod?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        successMsg.text = "Your authorization of $\(invoice?.total ?? 0) was successful"
+        successMsg.sizeToFit()
+        activitySpinner.isHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func voidAuthorization(_ sender: UIButton) {
+        activitySpinner.isHidden = false
+        activitySpinner.startAnimating()
+        
+        PayPalRetailSDK.transactionManager().voidAuthorization(authTransactionNumber) { (error) in
+            if let err = error {
+                print("Error Code: \(err.code)")
+                print("Error Message: \(err.message)")
+                print("Debug ID: \(err.debugId)")
+                self.activitySpinner.stopAnimating()
+                return
+            }
+
+            self.activitySpinner.stopAnimating()
+            self.activitySpinner.isHidden = true
+            self.voidAuthBtn.setImage(#imageLiteral(resourceName: "small-greenarrow"), for: .normal)
+            self.voidSuccessLbl.isHidden = false
+
+            self.captureAuthBtn.isEnabled = false
+            self.captureAuthBtn.setImage(#imageLiteral(resourceName: "small-grayarrow"), for: .disabled)
+
+            self.startOverBtn.isHidden = false
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "goToCaptureAuthView") {
+            if let captureAuthViewController = segue.destination as? CaptureAuthViewController {
+                captureAuthViewController.authTransactionNumber = authTransactionNumber
+                captureAuthViewController.invoice = invoice
+                captureAuthViewController.paymentMethod = paymentMethod
+            }
+        }
+    }
+    
+    @IBAction func captureAuthorization(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToCaptureAuthView", sender: sender)
+    }
+    
+    @IBAction func showInfo(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            if (voidCodeViewer.isHidden) {
+                voidCodeViewBtn.setTitle("Hide Code", for: .normal)
+                voidCodeViewer.isHidden = false
+                voidCodeViewer.text = "PayPalRetailSDK.transactionManager().voidAuthorization(authTransactionNumber) { (error) in\n" +
+                                      "   <code to handle success/failure>\n" +
+                                      "}"
+            } else {
+                voidCodeViewBtn.setTitle("View Code", for: .normal)
+                voidCodeViewer.isHidden = true
+            }
+        case 1:
+            if (captureAuthCodeViewer.isHidden) {
+                captureAuthCodeViewBtn.setTitle("Hide Code", for: .normal)
+                captureAuthCodeViewer.isHidden = false
+                captureAuthCodeViewer.text = "PayPalRetailSDK.captureAuthorizedTransaction(authTransactionNumber, invoiceId: invoice.payPalId, totalAmount: amountToCapture, gratuityAmount: 0, currency: invoice.currency) { (error, captureId) in\n" +
+                                             "  <code to handle success/failure>\n" +
+                                             "}"
+            } else {
+                captureAuthCodeViewBtn.setTitle("View Code", for: .normal)
+                captureAuthCodeViewer.isHidden = true
+            }
+        default:
+            print("No Button Tag Found")
+        }
+    }
+    
+    @IBAction func startOver(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToPaymentsView", sender: sender)
+    }
+    
+    func getCurrentNavigationController() -> UINavigationController! {
+        return self.navigationController
+    }
+
+}
