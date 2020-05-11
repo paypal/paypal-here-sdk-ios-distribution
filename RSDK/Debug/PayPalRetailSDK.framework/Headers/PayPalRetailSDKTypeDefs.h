044968baacdd0d3535eed8857f85f518edb057fb
jenkins <android-jenkins@paypal.com>
2020-05-11 15:43:20 -0700

    build: stable version 2.1.0320122010

--- RSDK/Debug/PayPalRetailSDK.framework/Headers/PayPalRetailSDKTypeDefs.h
+++ RSDK/Debug/PayPalRetailSDK.framework/Headers/PayPalRetailSDKTypeDefs.h
@@ -193,7 +193,8 @@ typedef NS_ENUM(NSInteger, PPRetailInvoiceAction) {
   PPRetailInvoiceActionMore = 10,
   PPRetailInvoiceActionShare = 11,
   PPRetailInvoiceActionViewHistory = 12,
-  PPRetailInvoiceActionViewInvoice = 13
+  PPRetailInvoiceActionViewInvoice = 13,
+  PPRetailInvoiceActionQRCode = 14
 };
 
 /**
