4f4eb71f1f6d1b886d1e8e30de6cef7683572520
pbhandari <pbhandari@paypal.com>
2021-02-23 14:42:31 -0800

    indentation

--- SampleApp/PPHSDKSampleApp/WelcomeViewController.swift
+++ SampleApp/PPHSDKSampleApp/WelcomeViewController.swift
@@ -9,39 +9,31 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var goToInitPage: UIButton!
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.navigationBar.isHidden = true
-        self.navigationController?.navigationBar.tintColor = .darkGray
-        self.navigationController?.navigationBar.backIndicatorImage = #imageLiteral(resourceName: "Close")
-        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = #imageLiteral(resourceName: "Close")
-        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.navigationBar.isHidden = false
-    }
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func goToInitPage(_ sender: UIButton) {
-        
-        performSegue(withIdentifier: "showInitPageSegue", sender: sender)
-    }
-
-
+  @IBOutlet weak var goToInitPage: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    configureNavigationController()
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    navigationController?.navigationBar.isHidden = false
+  }
+  
+  private func configureNavigationController() {
+    navigationController?.navigationBar.isHidden = true
+    navigationController?.navigationBar.tintColor = .darkGray
+    navigationController?.navigationBar.backIndicatorImage = #imageLiteral(resourceName: "Close")
+    navigationController?.navigationBar.backIndicatorTransitionMaskImage = #imageLiteral(resourceName: "Close")
+    navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+  }
+  
+  @IBAction func goToInitPage(_ sender: UIButton) {
+    performSegue(withIdentifier: "showInitPageSegue", sender: sender)
+  }
 }
