a83f9aed0111bd0a017a1edb1778ae7bbca6289a
pbhandari <pbhandari@paypal.com>
2021-03-01 14:55:30 -0800

    welcome view controller refactoring

---
+++ SampleApp/PPHSDKSampleApp/UICatalog/Theme.swift
@@ -0,0 +1,46 @@
+//
+//  Theme.swift
+//  PPHSDKSampleApp
+//
+//  Created by Pranav Bhandari on 2/23/21.
+//  Copyright © 2021 cowright. All rights reserved.
+//
+
+import UIKit
+
+enum PPHTheme {
+  
+  /// PayPalSansBig-Light font, 17 pt font size
+  case sansBigLight
+  
+  /// PayPalSansBig-Regular font, 17 pt font size
+  case sansBigRegular
+  
+  var fontColor: UIColor {
+    switch self {
+    case .sansBigLight:
+      return PPHColor.black
+      
+    case .sansBigRegular:
+      return PPHColor.black
+    }
+    
+  }
+  
+  var fontSize: CGFloat {
+    switch self {
+    case .sansBigLight, .sansBigRegular:
+      return 17.0
+    }
+  }
+  
+  var fontFamily: String {
+    switch self {
+    case .sansBigLight:
+      return "PayPalSansBig-Light"
+      
+    case .sansBigRegular:
+      return "PayPalSansBig-Regular"
+    }
+  }
+}
--- SampleApp/PPHSDKSampleApp/WelcomeViewController.swift
+++ SampleApp/PPHSDKSampleApp/WelcomeViewController.swift
@@ -9,10 +9,49 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-  @IBOutlet weak var goToInitPage: UIButton!
+  private var imgView: UIImageView = {
+    let imgView = UIImageView()
+    imgView.translatesAutoresizingMaskIntoConstraints = false
+    imgView.image = UIImage(named: "PPHere-Logo")
+    imgView.contentMode = .scaleToFill
+    return imgView
+  }()
+  
+  private var sdkVersionLabel: UILabel = {
+    let lbl = UILabel()
+    lbl.translatesAutoresizingMaskIntoConstraints = false
+    lbl.textAlignment = .center
+    lbl.text = "SDK v2.x"
+    lbl.applyTheme(theme: .sansBigLight)
+    return lbl
+  }()
+  
+  private var demoAppLabel: UILabel = {
+    let lbl = UILabel()
+    lbl.translatesAutoresizingMaskIntoConstraints = false
+    lbl.textAlignment = .center
+    lbl.text = "Demo App"
+    lbl.applyTheme(theme: .sansBigLight)
+    return lbl
+  }()
+  
+  private var getStartedButton: UIButton = {
+    let btn = UIButton()
+    btn.translatesAutoresizingMaskIntoConstraints = false
+    btn.setTitle("Get Started", for: .normal)
+    btn.titleLabel?.applyTheme(theme: .sansBigRegular)
+    btn.backgroundColor = PPHColor.azure
+    btn.setTitleColor(.white, for: .normal)
+    btn.layer.cornerRadius = 24
+    return btn
+  }()
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    layoutImgView()
+    layoutSDKVersionLabel()
+    layoutDemoAppLabel()
+    layoutGetStartedButton()
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -33,7 +72,42 @@ class WelcomeViewController: UIViewController {
     navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
   }
   
-  @IBAction func goToInitPage(_ sender: UIButton) {
+  private func layoutImgView() {
+    view.addSubview(imgView)
+
+    imgView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    imgView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    imgView.heightAnchor.constraint(equalToConstant: 175).isActive = true
+  }
+  
+  private func layoutSDKVersionLabel() {
+    view.addSubview(sdkVersionLabel)
+    
+    sdkVersionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+    sdkVersionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+    sdkVersionLabel.topAnchor.constraint(equalTo: imgView.bottomAnchor, constant: 4).isActive = true
+  }
+  
+  private func layoutDemoAppLabel() {
+    view.addSubview(demoAppLabel)
+    
+    demoAppLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+    demoAppLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+    demoAppLabel.topAnchor.constraint(equalTo: sdkVersionLabel.bottomAnchor, constant: 4).isActive = true
+    demoAppLabel.heightAnchor.constraint(equalToConstant: 24).isActive = true
+  }
+  
+  private func layoutGetStartedButton() {
+    getStartedButton.addTarget(self, action: #selector(goToInitPage(_:)), for: .touchUpInside)
+    view.addSubview(getStartedButton)
+    
+    getStartedButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+    getStartedButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+    getStartedButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -24).isActive = true
+    getStartedButton.heightAnchor.constraint(equalToConstant: 48).isActive = true
+  }
+  
+  @objc func goToInitPage(_ sender: UIButton) {
     performSegue(withIdentifier: "showInitPageSegue", sender: sender)
   }
 }
